390319b3e6c36dbf4b0ea0bc7a8dd40fd9747c76
dukcode <duk9741@gmail.com>
2023-04-05 11:51:01 +0900

    chap03: 여전한 데이터 중심의 설계

--- src/main/java/org/example/object/chap03/ReservationAgency.java
+++ src/main/java/org/example/object/chap03/ReservationAgency.java
@@ -2,43 +2,7 @@ package org.example.object.chap03;
 
 public class ReservationAgency {
 	public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-		Movie movie = screening.getMovie();
-
-		boolean discountable = false;
-		for (DiscountCondition condition : movie.getDiscountConditions()) {
-			if (condition.getType() == DiscountConditionType.PERIOD) {
-				discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
-					condition.getStartTime().isBefore(screening.getWhenScreened().toLocalTime()) &&
-					condition.getEndTime().isAfter(screening.getWhenScreened().toLocalTime());
-			} else {
-				discountable = condition.getSequence() == condition.getSequence();
-			}
-
-			if (discountable) {
-				break;
-			}
-		}
-
-		Money fee;
-		if (discountable) {
-			Money discountAmount = Money.ZERO;
-			switch (movie.getMovieType()) {
-				case AMOUNT_DISCOUNT:
-					discountAmount = movie.getDiscountAmount();
-					break;
-				case PERCENT_DISCOUNT:
-					discountAmount = movie.getFee().times(movie.getDiscountPercent());
-					break;
-				case NONE_DISCOUNT:
-					break;
-			}
-
-			fee = movie.getFee().minus(discountAmount);
-		} else {
-			fee = movie.getFee();
-		}
-
+		Money fee = screening.calculateFee(audienceCount);
 		return new Reservation(customer, screening, fee, audienceCount);
-
 	}
 }
