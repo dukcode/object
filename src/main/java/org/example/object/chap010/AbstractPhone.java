f20abb5dbe3932ac338e230c74e386fc87f1846e
dukcode <duk9741@gmail.com>
2023-04-06 23:18:53 +0900

    chap10: 추상화에 기댄 상속

---
+++ src/main/java/org/example/object/chap010/AbstractPhone.java
@@ -0,0 +1,21 @@
+package org.example.object.chap010;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractPhone {
+
+	private List<Call> calls = new ArrayList<>();
+
+	public Money calculateFee() {
+		Money result = Money.ZERO;
+
+		for (Call call : calls) {
+			result = calculateCallFee(call);
+		}
+
+		return result;
+	}
+
+	abstract protected Money calculateCallFee(Call call);
+}
