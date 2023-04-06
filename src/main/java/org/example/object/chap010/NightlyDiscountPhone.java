b38a797420a511e81a36ba934aed7a21f93cfac8
dukcode <duk9741@gmail.com>
2023-04-06 18:35:21 +0900

    chap10: 강하게 결합된 부모클래스와 자식클래스 원복
    This reverts commit 674da67b1bfa16d2f4c874015dd8423657265b43.

--- src/main/java/org/example/object/chap010/NightlyDiscountPhone.java
+++ src/main/java/org/example/object/chap010/NightlyDiscountPhone.java
@@ -1,31 +1,39 @@
 package org.example.object.chap010;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NightlyDiscountPhone extends Phone {
+public class NightlyDiscountPhone {
 
 	private static final int LATE_NIGHT_HOUR = 22;
 
 	private Money nightlyAmount;
+	private Money regularAmount;
+	private Duration seconds;
+	private List<Call> calls = new ArrayList<>();
 
 	public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
-		super(regularAmount, seconds);
 		this.nightlyAmount = nightlyAmount;
+		this.regularAmount = regularAmount;
+		this.seconds = seconds;
+	}
+
+	public void call(Call call) {
+		calls.add(call);
 	}
 
 	public Money calculateFee() {
-		Money result = super.calculateFee();
+		Money result = Money.ZERO;
 
-		Money nightFee = Money.ZERO;
-		for (Call call : getCalls()) {
+		for (Call call : calls) {
 			if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-				nightFee = nightFee.plus(
-					getAmount().minus(nightlyAmount).times(
-						call.getDuration().getSeconds() / getSeconds().getSeconds()
-					));
+				result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+			} else {
+				result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
 			}
 		}
 
-		return result.minus(nightFee);
+		return result;
 	}
 }
