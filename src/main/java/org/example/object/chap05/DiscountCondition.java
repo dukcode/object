072824fd26ff75f618fee17110f6692db688346d
dukcode <duk9741@gmail.com>
2023-04-06 18:13:05 +0900

    Revert "chap06: POLYMORPHISM 패턴과 PROTECTED VARTIATIONS 적용"
    This reverts commit 7334cecbb5deaaf37b71c176de92a5d76da67918.

--- src/main/java/org/example/object/chap05/DiscountCondition.java
+++ src/main/java/org/example/object/chap05/DiscountCondition.java
@@ -1,7 +1,33 @@
 package org.example.object.chap05;
 
-public interface DiscountCondition {
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 
-	boolean isSatisfiedBy(Screening screening);
+public class DiscountCondition {
 
+	private DiscountConditionType type;
+
+	private int sequence;
+
+	private DayOfWeek dayOfWeek;
+	private LocalTime startTime;
+	private LocalTime endTime;
+
+	boolean isSatisfiedBy(Screening screening) {
+		if (type == DiscountConditionType.PERIOD) {
+			return isSatisfiedByPeriod(screening);
+		}
+
+		return isSatisfiedBySequence(screening);
+	}
+
+	private boolean isSatisfiedBySequence(Screening screening) {
+		return sequence == screening.getSequence();
+	}
+
+	private boolean isSatisfiedByPeriod(Screening screening) {
+		return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
+			startTime.isBefore(screening.getWhenScreened().toLocalTime()) &&
+			endTime.isAfter(screening.getWhenScreened().toLocalTime());
+	}
 }
--- src/main/java/org/example/object/chap05/Movie.java
+++ src/main/java/org/example/object/chap05/Movie.java
@@ -3,35 +3,52 @@ package org.example.object.chap05;
 import java.time.Duration;
 import java.util.List;
 
-public abstract class Movie {
+public class Movie {
 
 	private String title;
 	private Duration runningTime;
 	private Money fee;
 	private List<DiscountCondition> discountConditions;
 
-	public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
-		this.title = title;
-		this.runningTime = runningTime;
-		this.fee = fee;
-		this.discountConditions = List.of(discountConditions);
-	}
+	private MovieType movieType;
+	private Money discountAmount;
+	private double discountPercent;
 
 	public Money calculateMovieFee(Screening screening) {
 		if (isDiscountable(screening)) {
-			return fee.minus(calculateDiscountAmount());
+			return fee.minus(calculateDiscountAmount(screening));
 		}
 
 		return fee;
 	}
 
-	protected abstract Money calculateDiscountAmount();
+	private Money calculateDiscountAmount(Screening screening) {
+		switch (movieType) {
+			case AMOUNT_DISCOUNT:
+				return calculateAmountDiscountAmount();
+			case PERCENT_DISCOUNT:
+				return calculatePercentDiscountAmount();
+			case NONE_DISCOUNT:
+				return calculateNoneDiscountAmount();
+		}
+
+		throw new IllegalStateException();
+	}
+
+	private Money calculateAmountDiscountAmount() {
+		return discountAmount;
+	}
+
+	private Money calculatePercentDiscountAmount() {
+		return fee.times(discountPercent);
+	}
+
+	private Money calculateNoneDiscountAmount() {
+		return Money.ZERO;
+	}
 
 	private boolean isDiscountable(Screening screening) {
 		return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
 	}
 
-	public Money getFee() {
-		return fee;
-	}
 }
